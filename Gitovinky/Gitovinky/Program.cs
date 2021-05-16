63589494b7e8b6e09fd5b92a4e19721a13a67404
Daniel <dkol.kollar@gmail.com>
2021-05-16 16:42:40 +0200

    druha uprava

--- Gitovinky/Gitovinky/Program.cs
+++ Gitovinky/Gitovinky/Program.cs
@@ -11,6 +11,7 @@ namespace Gitovinky
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Prva zmena súboru");
         }
     }
 }
