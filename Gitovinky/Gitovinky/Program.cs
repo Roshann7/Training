6e7e533a95f451c06b2de63f49caa080596840c5
Daniel <dkol.kollar@gmail.com>
2021-05-16 16:59:13 +0200

    Vsetko na druhej vetve

--- Gitovinky/Gitovinky/Program.cs
+++ Gitovinky/Gitovinky/Program.cs
@@ -11,6 +11,7 @@ namespace Gitovinky
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("druha vetev");
         }
     }
 }
